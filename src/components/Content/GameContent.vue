20f225ac0ad9198d01fa656fb4b68f04a858319a
blksy <79380937+blksy@users.noreply.github.com>
2023-05-14 18:09:53 +0200

    added basic data required for project to function properly, added a method that generates a random word from words array

--- src/components/Content/GameContent.vue
+++ src/components/Content/GameContent.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="content">
-        <h3>Type the given word within <span class="seconds">SECONDS</span> seconds</h3>
-        <h1 class="guess">WORD </h1>
+        <h3>Type the given word within <span class="seconds">{{seconds}}</span> seconds</h3>
+        <h1 class="guess">{{ word }}</h1>
             <form ref="form" >
                 <input type="text" placeholder="Start typing"/>
             </form>
         <h4 class="message">{{message}}</h4>
       <div class="score">
-        <h4>Time Left: TIME</h4>
-        <h4>Score: SCORE</h4>
+        <h4>Time Left: {{ time }}</h4>
+        <h4>Score: {{score}}</h4>
       </div>
       <button class="button">New Game</button>
     </div>
@@ -16,6 +16,27 @@
 
 <script>
    export default{
+    data(){
+        return{
+            time: 0,
+            score: 0,
+            seconds:0,
+            message: '',
+            word: '',
+            words:['hello','appointment','crossover','hat','river','statue','generate','stubborn','cocktail','sugar','nonsense','jealous',
+             'impatient','joke','appropriate','mountaintop','javascript','algorithm',"own","other","old","right","big","high","different","small",
+             "large","next","early","young","important","few","public","bad","same","able","disability","arrangement","impostor","credibility"],
+        }
+    },
+    methods:{
+        randomWord(){
+            let randomWord = Math.floor(Math.random() * this.words.length)
+            return this.words[randomWord];
+        }
+    },
+    mounted(){
+        this.word = this.randomWord();
+    }
    }
 </script>
 
